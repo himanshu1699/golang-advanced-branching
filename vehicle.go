88f91138b557812c8eae2ac1bb86ccbd5399d094
himanshu1699 <65755812+himanshu1699@users.noreply.github.com>
2020-06-05 00:58:34 +0530

    First Commit

--- vehicle.go
+++ vehicle.go
@@ -4,10 +4,50 @@ import (
 
 )
 
+type vehicle interface{
+
+}
+type car struct {
+
+     	model string
+   	    make string
+   	    typeVehicle string
+
+}
+type truck struct {
+
+	   model string
+	   make string
+	   typeVehicle string
+
+}
+type bike struct {
+
+	   model string
+	   make string
+
+}
+
+
 
 // Values array for the feedback.json file
+type Values struct {
+     Models []Model = json:"values"
+}
 
 // Model array for the feedback.json file
+type Model struct {
+     Name string = json:"model"
+     Feedback []string = json:"feedback"
+}
+
+type feedbackResult struct {
+	var feedbackTotal int
+	var feedbackPositive int
+	var feedbackNegative int
+	var feedbackNeutral int
+
+}
 
 type rating float32
 
@@ -21,8 +61,8 @@ const (
 
 func init() {
 
-/*
-	inventory = []vehicle{
+
+	inventory := []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
 		car{"Sonata", "Hyundai", "Sedan"},
@@ -34,8 +74,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult := make(map[string]feedbackResult)
 
 }
 
